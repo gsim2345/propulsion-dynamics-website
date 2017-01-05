642785306e21ebb0a624869f915b48b03ed2e01e
Gabriella Simon <gb@gabriellasimon.me>
2017-01-05 10:56:25 +0100

    Navbar height adjustment

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ $(document).ready(function() {
   //Waypoints
       $('#service-info').waypoint(function(direction) {
           if (direction == 'down') {
-              $('#navigation-bar').css('background-color', '#6ABCBC');
+              $('#navigation nav').css('background-color', '#6ABCBC');
           }
           if (direction == 'up') {
-              $('#navigation-bar').css('background-color', 'transparent');
+              $('#navigation nav').css('background-color', 'transparent');
           }
       }, {
           offset: 25
