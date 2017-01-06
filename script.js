5a7e95abce9e036fdff0ce14a2c22593e3283108
Gabriella Simon <gb@gabriellasimon.me>
2017-01-06 11:52:40 +0100

    Change colors

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
   //Waypoints
       $('#service-info').waypoint(function(direction) {
           if (direction == 'down') {
-              $('#navigation nav').css('background-color', '#6ABCBC');
+              $('#navigation nav').css('background-color', '#1d6989');
           }
           if (direction == 'up') {
               $('#navigation nav').css('background-color', 'transparent');
